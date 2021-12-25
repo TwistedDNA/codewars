25a67031a974acf0f50bb16177e6d51aa72264e2
TwistedDNA <legols93@gmail.com>
2021-12-25 22:01:04 +0100

    Kata: [kyu 5] Count IP addresses, part 1

---
+++ src/active/learning/count/ip/CountIPAddresses.java
@@ -0,0 +1,8 @@
+package active.learning.count.ip;
+
+public class CountIPAddresses {
+    public static long ipsBetween(String start, String end) {
+        // TODO solve
+        return -1;
+    }
+}
