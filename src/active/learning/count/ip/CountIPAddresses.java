1e0a6d9c6df2fce37dcd041a4f42fd3c3ed2df33
TwistedDNA <legols93@gmail.com>
2021-12-30 12:50:22 +0100

    Kata: [kyu 5] Count IP addresses, part 2, competed.

--- src/active/learning/count/ip/CountIPAddresses.java
+++ src/active/learning/count/ip/CountIPAddresses.java
@@ -2,7 +2,13 @@ package active.learning.count.ip;
 
 public class CountIPAddresses {
     public static long ipsBetween(String start, String end) {
-        // TODO solve
-        return -1;
+        String[] from = start.split("\\.");
+        String[] to = end.split("\\.");
+        int result = 0;
+        for (int power = 0; power < 4; power++) {
+            int pow = (int) Math.pow(256, power);
+            result += pow * (Integer.parseInt(to[3 - power]) - Integer.parseInt(from[3 - power]));
+        }
+        return result;
     }
 }
--- test/active/learning/count/ip/CountIPAddressesTest.java
+++ test/active/learning/count/ip/CountIPAddressesTest.java
@@ -6,9 +6,16 @@ import static org.junit.Assert.*;
 
 public class CountIPAddressesTest {
     @Test
-    public void ipsBetween() throws Exception {
+    public void test01() {
         assertEquals( 50, CountIPAddresses.ipsBetween( "10.0.0.0", "10.0.0.50" ) );
+    }
+    @Test
+    public void test02(){
         assertEquals( 246, CountIPAddresses.ipsBetween( "20.0.0.10", "20.0.1.0" ) );
+    }
+    @Test
+    public void test03(){
+        assertEquals( 255, CountIPAddresses.ipsBetween( "20.0.0.1", "20.0.1.0" ) );
     }
 
 }
