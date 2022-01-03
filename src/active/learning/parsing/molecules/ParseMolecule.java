2a1642fd68a4d7889741c07834b2ad31573a1a2c
TwistedDNA <legols93@gmail.com>
2022-01-03 15:15:25 +0100

    Kata: [kyu 5] Parse molecule pt1

---
+++ src/active/learning/parsing/molecules/ParseMolecule.java
@@ -0,0 +1,11 @@
+package active.learning.parsing.molecules;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ParseMolecule {
+    public static Map<String,Integer> getAtoms(String formula) {
+        // Your code here!
+        return new HashMap<String,Integer>();
+    }
+}
